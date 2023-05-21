428746d65adbc02688a35259c8c857e8a7320a79
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2023-05-21 22:01:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,26 @@
-from flask import Flask, render_template, request
+import sys
 import os
+from flask import Flask, render_template, request
 import aiml
 from autocorrect import Speller
-from AppKit import NSSpeechSynthesizer #Habilitar para MAC
-#import pyttsx3  //Habilitar para OS Windows
-#from bs4 import BeautifulSoup 
+
+if sys.platform.startswith('darwin'):
+    from AppKit import NSSpeechSynthesizer # Sintetizador de voz para MacOS
+        
+    synthesizer = NSSpeechSynthesizer.alloc().init()
+    synthesizer.setRate_(90) # Velocidad por defecto del sintetizador
+elif sys.platform.startswith('win') or sys.platform.startswith('linux'):
+    import pyttsx3 # Sintetizador de voz para Windows y Linux
+
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', 'spanish')
+
 
 app = Flask(__name__)
 
 BRAIN_FILE="./pretrained_model/aiml_pretrained_model.dump"
 k = aiml.Kernel()
-
-synthesizer = NSSpeechSynthesizer.alloc().init()
-synthesizer.setRate_(90)
-
-'''
-html = '<p>Hola! Soy CoffeeBot, tu asistente virtual del Parque Del Café. Estoy aquí para darte una mano y resolver las dudas que puedas tener!</p>'
-soup = BeautifulSoup(html, 'html.parser')
-texto = soup.get_text()
-'''
-#engine = pyttsx3.init()
-'''
-engine.setProperty('voice', 'spanish')
-engine.say(texto)
-'''
-#engine.setProperty('voice', 'spanish')
 
 if os.path.exists(BRAIN_FILE):
     print("Loading from brain file: " + BRAIN_FILE)
@@ -40,12 +36,26 @@ else:
 def home():
     return render_template("home.html")
 
-
 @app.route("/actualizar_velocidad", methods=["POST"])
 def actualizar_velocidad():
     data = request.get_json()
     velocidad = int(data["velocidad"])
-    synthesizer.setRate_(velocidad)
+    
+    if sys.platform.startswith('darwin'):
+        synthesizer.setRate_(velocidad)
+    elif sys.platform.startswith('win') or sys.platform.startswith('linux'):
+        # Mapear la velocidad a los valores admitidos por pyttsx3
+        if velocidad == 60:
+            velocidadPyttsx3 = 100  # Configuración de velocidad mínima en pyttsx3
+        elif velocidad == 90:
+            velocidadPyttsx3 = 200  # Configuración de velocidad predeterminada en pyttsx3
+        elif velocidad == 100:
+            velocidadPyttsx3 = 300
+        elif velocidad == 150:
+            velocidadPyttsx3 = 400
+
+        engine.setProperty('rate', velocidadPyttsx3)
+
     print(velocidad)
     return {"status": "ok"}
 
@@ -54,20 +64,25 @@ def get_bot_response():
     pr = Speller(lang='es')
     query = request.args.get('msg')
     query= pr(query)
-    #query = [Speller().autocorrect_word(w) for w in (query.split())]
     print(query)
     response = k.respond(query)
     if response:
-        synthesizer.startSpeakingString_(response)
-        #os.system('echo "' + response + '" | festival --tts')
-        #engine.say(response)
-        #engine.runAndWait()
+        
+        if sys.platform.startswith('darwin'):
+            synthesizer.startSpeakingString_(response)
+        elif sys.platform.startswith('win') or sys.platform.startswith('linux'):
+            engine.say(str(response))
+            engine.runAndWait()
         
         return (str(response))
     else:
         response = "Hola! En el menor tiempo posible nos comunicaremos  contigo y resolveremos todas tus inquietudes. También puedes encontrar la información que necesitas en nuestra página web www.parquedelcafe.co"
-        #engine.say(str(response))
-        #engine.runAndWait()
+        
+        if sys.platform.startswith('darwin'):
+            synthesizer.startSpeakingString_(response)
+        elif sys.platform.startswith('win') or sys.platform.startswith('linux'):
+            engine.say(str(response))
+            engine.runAndWait()
         return (str(response))
 
 
